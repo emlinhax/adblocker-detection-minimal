1b843b3343ebfd21cb73fe18b829cfd632a1f64a
Emlin <49819627+R4YVEN@users.noreply.github.com>
2022-03-01 15:01:58 +0100

    Update detection.js

--- detection.js
+++ detection.js
@@ -1,1 +1,2 @@
+//will return true if adblocker is detected
 function detect_adblock(){try{var e=new XMLHttpRequest;return e.open("GET","https://securepubads.g.doubleclick.net/pagead/ppub_config",!1),e.send(null),!1}catch{return!0}}

feadf0f5cadbf365586803467a11b07cf66a638d
Emlin <49819627+R4YVEN@users.noreply.github.com>
2022-03-01 15:05:39 +0100

    Update detection.js

--- detection.js
+++ detection.js
@@ -1,7 +1,1 @@
-//randomize script name + function name for full protection
-//function will return true if adblocker is detected
 function detect_adblock(){try{var e=new XMLHttpRequest;return e.open("GET","https://securepubads.g.doubleclick.net/pagead/ppub_config",!1),e.send(null),!1}catch{return!0}}
-
-//other links you can use:
-//  - https://ads.stickyadstv.com/user-registering
-//  - https://static.criteo.net/images/pixel.gif?ch=2

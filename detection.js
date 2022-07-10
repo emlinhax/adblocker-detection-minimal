70b266493992f0bba26fb5f79a35e03d3df02028
Emlin <49819627+R4YVEN@users.noreply.github.com>
2022-07-10 16:33:11 +0200

    Update detection.js

--- detection.js
+++ detection.js
@@ -1,1 +1,10 @@
-function detect_adblock(){try{var e=new XMLHttpRequest;return e.open("GET","https://securepubads.g.doubleclick.net/pagead/ppub_config",!1),e.send(null),!1}catch{return!0}}
+function detect_adblock(){
+  try{
+    var e = new XMLHttpRequest;
+    e.open("GET","https://securepubads.g.doubleclick.net/pagead/ppub_config",0);
+    e.send(null);
+    return 0;
+  } catch {
+    return 1
+  }
+}
